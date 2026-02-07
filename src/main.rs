daed1692e0b1dc7807340659fc96e3072140c530
gordonmcvey <grj.mcvey@gmail.com>
2026-02-07 08:15:22 +0000

    Print out all colours in the terminal CLUT

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use colored::Colorize;
+
+const SYMBOL: char = '▚';
+
 fn main() {
-    println!("Hello, world!");
+    draw_all();
+}
+
+fn draw_all() {
+    println!("All colours:");
+    let test = SYMBOL.to_string();
+    for x in 0..=15 {
+        print!("{}", test.ansi_color(x));
+    }
+    println!();
+
+    for x in 16..=231 {
+        print!("{}", test.ansi_color(x));
+        if (x - 16) % 36 == 35 {
+            println!();
+        }
+    }
+    for x in 232..=255 {
+        print!("{}", test.ansi_color(x));
+    }
+    println!();
 }
